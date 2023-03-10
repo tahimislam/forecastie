ac08875537e7ad50c76f17915be96a94fd3f8988
tahimislam <tahimislam16@gmail.com>
2023-03-11 03:08:16 +0600

    change in main branch

---
+++ app/src/main/java/cz/martykan/forecastie/activities/demo.java
@@ -0,0 +1,26 @@
+package cz.martykan.forecastie.activities;
+
+public class demo {
+    private String test1,test2;
+
+    public demo(String test1, String test2) {
+        this.test1 = test1;
+        this.test2 = test2;
+    }
+
+    public String getTest1() {
+        return test1;
+    }
+
+    public String getTest2() {
+        return test2;
+    }
+
+    public void setTest1(String test1) {
+        this.test1 = test1;
+    }
+
+    public void setTest2(String test2) {
+        this.test2 = test2;
+    }
+}
